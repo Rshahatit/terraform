5c7566988d9e935281e6386fa7a116ee6adc638a
Rami Shahatit <rshahatit@berkeley.edu>
2020-09-25 15:02:04 -0700

    successfully deployed multi cluster of webservers behind a lb
    Change-Id: I4180740421b0f47a5933d6a3ee5f8cf4b2333559

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-2"
 }
 resource "aws_launch_configuration" "example" {
-  iamge_id        = "ami-0c55b159cbfafe1f0"
+  image_id        = "ami-0c55b159cbfafe1f0"
   instance_type   = "t2.micro"
   security_groups = [aws_security_group.instance.id]
 
@@ -21,7 +21,7 @@ resource "aws_launch_configuration" "example" {
 
 resource "aws_autoscaling_group" "example" {
   launch_configuration = aws_launch_configuration.example.name
-  vpc_zone_identifier  = data.aws_subnet_ids.default.aws_subnet_ids
+  vpc_zone_identifier  = data.aws_subnet_ids.default.ids
 
   target_group_arns = [aws_lb_target_group.asg.arn]
   health_check_type = "ELB"
@@ -37,58 +37,58 @@ resource "aws_autoscaling_group" "example" {
 }
 
 resource "aws_lb" "example" {
-    name = "terraform-asg-example"
-    load_balancer_type = "application"
-    subnets = data.aws_subnet_ids.default.ids
-    security_groups = [aws_security_group.alb.id]
+  name = "terraform-asg-example"
+  load_balancer_type = "application"
+  subnets = data.aws_subnet_ids.default.ids
+  security_groups = [aws_security_group.alb.id]
 }
 
 resource "aws_lb_target_group" "asg" {
-    name = "terraform-asg-example"
-    port = var.server_port
-    protocol = "HTTP"
-    vpc_id = data.aws_vpc.default.id
+  name = "terraform-asg-example"
+  port = var.server_port
+  protocol = "HTTP"
+  vpc_id = data.aws_vpc.default.id
 
-    health_check {
-        path = "/" 
-        protocol = "HTTP" 
-        matcher = "200"
-        interval = 15 
-        timeout = 3
-        healthy_threshold = 2
-        unhealthy_threshold = 2
-    }
+  health_check {
+    path = "/" 
+    protocol = "HTTP" 
+    matcher = "200"
+    interval = 15 
+    timeout = 3
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+  }
 }
 resource "aws_lb_listener" "http" {
-    load_balancer_arn = aws_lb.example.arn
-    port = 80
-    protocol = "HTTP"
+  load_balancer_arn = aws_lb.example.arn
+  port = 80
+  protocol = "HTTP"
 
-    default_action {
-        type = "fixed-response"
+  default_action {
+    type = "fixed-response"
 
-        fixed_response {
-            content_type = "text/plain"
-            message)body = "404: page not found"
-            status_code = 404
-        }
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code = 404
     }
+  }
 }
 
 resource "aws_lb_listener_rule" "asg" {
-    listener_arn = aws_lb_listener.http.arn 
-    priority = 100
+  listener_arn = aws_lb_listener.http.arn 
+  priority = 100
 
-    condition {
-        path_pattern {
-            values = [*]
-        }
+  condition {
+    path_pattern {
+      values = ["*"]
     }
+  }
 
-    action {
-        type = "forward"
-        target_group_arn = aws_lb_target_group.asg.arn 
-    }
+  action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn 
+  }
 }
 
 resource "aws_security_group" "alb" {
@@ -122,9 +122,10 @@ resource "aws_security_group" "instance" {
   }
 }
 
-output "public_ip" {
-  value       = aws_instance.example.public_ip
-  description = "The public IP address of the web server"
+
+output "alb_dns_name" {
+  value       = aws_lb.example.dns_name
+  description = "The domain name of the load balancer"
 }
 
 variable "server_port" {
